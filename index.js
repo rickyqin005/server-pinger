be83a631e5649ddc8c40dfd1c6f3ab61eea80d4f
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2024-08-14 21:19:29 -0400

    create endpoint

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
 const axios = require('axios');
+const express = require('express');
 
 // ping every 10 minutes
 setInterval(async () => {
     await axios.get(process.env.SERVER_URL);
     console.log(`pinged ${process.env.SERVER_URL}`);
 }, 600000);
+
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+app.get('/api/check', (req, res) => {
+    console.log(`GET ${req.path}`);
+    res.send('Hello');
+});
